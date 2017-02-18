1bbfd2da865c3ada312596e1edeccc5c8b5495f0
Andrew Wanex <awanex2705@gmail.com>
2017-02-18 18:02:12 +0000

    Combined functions and test

--- create_box/main.py
+++ create_box/main.py
@@ -1,6 +1,14 @@
 """This is the entry point of the program."""
 
 def create_box(height, width, char):
+    count = 0
+    end_str = ''
+    while count < height:
+        end_str += char * width + '\n'
+        count += 1
+    return end_str   
+
+def create_empty_box(height, width, char):
     if height < 1 or width < 1:
         return 'Invalid dimensions, height and/or width must be at least equal to 1.'
     count = 0
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import unittest
 
 from create_box import create_box
+from create_box import create_empty_box
 
 first_box_expected = """
 ****
@@ -24,9 +25,15 @@ error_expected = 'Invalid dimensions, height and/or width must be at least equal
 
 
 class TestCreateBox(unittest.TestCase):
+    def test_box(self):
+        self.assertEqual(create_box(3, 4, '*'), first_box_expected)
+
+    def test_small_box(self):
+        self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
     def test_empty_box(self):
-        self.assertEqual(create_box(5, 5, '$'), third_box_expected)
+        self.assertEqual(create_empty_box(5, 5, '$'), third_box_expected)
         
     def test_empty_box_error(self):
-        self.assertEqual(create_box(0, 0, '#'), error_expected)
+        self.assertEqual(create_empty_box(0, 0, '#'), error_expected)
 
