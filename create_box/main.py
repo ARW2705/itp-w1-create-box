5ccb0b8dfeeb67f43347c28a615c475e2f5e9006
Andrew Wanex <awanex2705@gmail.com>
2017-02-18 17:29:54 +0000

    Initial commit

--- create_box/main.py
+++ create_box/main.py
@@ -3,20 +3,13 @@
 # Original objective
 
 def create_box(height, width, char):
-    if height < 1 or width < 1:
-        return 'Invalid dimensions, height and/or width must be at least equal to 1.'
     count = 0
-    end_str = '\n'
+    end_str = ''
     while count < height:
-        if count == 0 or count == height - 1:
-            end_str += char * width + '\n'
-            count += 1
-        else:
-            end_str += char + (' ' * (width - 2)) + char + '\n'
-            count += 1
-    print end_str        
-    return end_str
+        end_str += char * width + '\n'
+        count += 1
+    return end_str   
 
 
 if __name__ == '__main__':
-    create_box(5, 5, '*')
+    create_box(3, 4, '*')
